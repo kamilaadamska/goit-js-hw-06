7a037c9c80b31a394b04ef96aa5d955fa68d92e9
Kamila Adamska <121kama121@gmail.com>
2023-03-23 01:40:03 +0100

    Task 01 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,8 @@
-const listEl = document.getElementById("#categories");
-console.log(listEl);
+const listItemEl = document.querySelectorAll('.item');
+
+console.log('Number of categories:', listItemEl.length);
+
+listItemEl.forEach(element => {
+  console.log('Category', element.firstElementChild.innerText);
+  console.log('Elements', element.lastElementChild.children.length);
+});
