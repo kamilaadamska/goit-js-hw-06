6d1bfb74438c347c4dd72bc3c46c32d6bf4458ec
Kamila Adamska <121kama121@gmail.com>
2023-03-24 22:01:28 +0100

    Finished Task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,9 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const listEl = document.getElementById('ingredients');
+
+ingredients.forEach(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.innerText = ingredient;
+  itemEl.classList.add('item');
+  listEl.append(itemEl);
+});
