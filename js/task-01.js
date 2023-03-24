ac61591a620568418ba14bffd254e3ba03da0e17
Kamila Adamska <121kama121@gmail.com>
2023-03-24 20:55:09 +0100

    Added dots to task 01

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,6 @@ const listItemEl = document.querySelectorAll('.item');
 console.log('Number of categories:', listItemEl.length);
 
 listItemEl.forEach(element => {
-  console.log('Category', element.firstElementChild.innerText);
-  console.log('Elements', element.lastElementChild.children.length);
+  console.log('Category:', element.firstElementChild.innerText);
+  console.log('Elements:', element.lastElementChild.children.length);
 });
