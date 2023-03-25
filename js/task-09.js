20bac3482e83a7dcb9e04d752394ffa798c94e74
Kamila Adamska <121kama121@gmail.com>
2023-03-25 15:59:00 +0100

    Task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnEl.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.innerText = getRandomHexColor();
+});
