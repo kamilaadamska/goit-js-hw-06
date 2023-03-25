98f10fa7b6923ecc6d8e244cbb173c5d936bfae7
Kamila Adamska <121kama121@gmail.com>
2023-03-25 15:43:20 +0100

    Finished task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { email, password } = formEl.elements;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill in all the fields!');
+  }
+
+  const formValues = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formValues);
+  formEl.reset();
+});
