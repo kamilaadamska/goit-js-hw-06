93d0889ada39d29a29b1e120defea154901c3e7e
Kamila Adamska <121kama121@gmail.com>
2023-03-26 22:00:00 +0200

    Bugfix task 07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputEl = document.getElementById('font-size-control');
 const textEl = document.getElementById('text');
 
+textEl.style.fontSize = `${inputEl.value}px`;
+
 inputEl.addEventListener('input', () => {
   textEl.style.fontSize = `${inputEl.value}px`;
 });
